c7f77be21882f09dd5faeb934561c5cc793d96d6
Blaze Vincent <blaze.vincent@hotmail.com>
2022-02-20 11:25:16 -0600

    fix happy earth colors

--- components/happyEarths.js
+++ components/happyEarths.js
@@ -4,7 +4,7 @@ export default function HappyEarths(){
   const borderColors = ['border-orange-200', 'border-lime-200', 'border-violet-200']
 
   return <div
-    className="flex md:flex-row flex-col justify-between w-full h-max max-w-screen-md m-auto md:gap-4 gap-0"
+    className="md:flex hidden md:flex-row flex-col justify-between w-full h-max max-w-screen-md m-auto md:gap-4 gap-0"
   >
     {[,,,].fill('').map((element, index) => {
       return <div key={index} className={`w-max max-w-[75%] md:relative h-auto relative rounded-full overflow-hidden border-2 border-solid ${borderColors[index]}`}  >
